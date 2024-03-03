8ba44d3ad92685e4a58798ad8e229f9026bcd818
abhishekamishra <abhishekamishra7@gmail.com>
2024-03-03 11:06:29 +0530

    Project Structure - intial commit.

---
+++ src/main/java/com/med/newmedical/NewMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.med.newmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/med/newmedical/NewMedicalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.med.newmedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hi");
+	}
+
+}
