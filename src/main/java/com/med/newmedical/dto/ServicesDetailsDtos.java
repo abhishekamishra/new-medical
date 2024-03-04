a306fa545b2adbd792b3d34131f020b17751c28d
abhishekamishra <abhishekamishra7@gmail.com>
2024-03-04 19:21:37 +0530

    REST call created for new-medical-services.

---
+++ src/main/java/com/med/newmedical/dto/ServicesDetailsDtos.java
@@ -0,0 +1,16 @@
+package com.med.newmedical.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ServicesDetailsDtos {
+
+    private Long id;
+    private String serviceName;
+}
